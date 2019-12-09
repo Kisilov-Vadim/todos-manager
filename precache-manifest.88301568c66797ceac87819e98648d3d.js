405a8fd7ebab488850d23d45c1db30dabf71d116
VadimK <djvadya16@gmail.com>
2019-12-09 20:11:29 +0200

    Updates

---
+++ precache-manifest.88301568c66797ceac87819e98648d3d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a79ef21564f7635772881918df13f4",
+    "url": "/react-todo-manager/index.html"
+  },
+  {
+    "revision": "cf60c7037ee8fa0ad0a2",
+    "url": "/react-todo-manager/static/css/main.60b9e341.chunk.css"
+  },
+  {
+    "revision": "ede7fad683fe0480685d",
+    "url": "/react-todo-manager/static/js/2.2db36a16.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-todo-manager/static/js/2.2db36a16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf60c7037ee8fa0ad0a2",
+    "url": "/react-todo-manager/static/js/main.9edc7468.chunk.js"
+  },
+  {
+    "revision": "33ef106cfa9bec03e85a",
+    "url": "/react-todo-manager/static/js/runtime-main.27739374.js"
+  },
+  {
+    "revision": "6c498791e52ee77eedea219f291f638d",
+    "url": "/react-todo-manager/static/media/SFProText-Heavy.6c498791.ttf"
+  },
+  {
+    "revision": "85bd46c1cff02c1d8360cc714b8298fa",
+    "url": "/react-todo-manager/static/media/SFProText-Regular.85bd46c1.ttf"
+  },
+  {
+    "revision": "1a131c948d598ecec700d37d168a15b5",
+    "url": "/react-todo-manager/static/media/SFProText-Semibold.1a131c94.ttf"
+  }
+]);
--- src/store/action.js
+++
@@ -1,9 +0,0 @@
-export const ACTIONS = {
-  ADD_TODO: 'ADD_TODO', 
-  REMOVE_TODO: 'REMOVE_TODO',
-  DELETE_TODO: 'DELETE_TODO'
-}
-
-export const addTodo = (todo) => ({ type: ACTIONS.ADD_TODO, todo }); 
-export const removeTodo = (id) => ({ type: ACTIONS.REMOVE_TODO, id });
-export const deleteTodo = (id) => ({ type: ACTIONS.DELETE_TODO, id }); 
